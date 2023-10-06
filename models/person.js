f9694266213cbb4a04c6531f25237d59fde14c06
Clemens Janes <clemens.a.janes@gmail.com>
2023-10-06 20:30:27 +0300

    Complete exercises 3.13 - 3.14

---
+++ models/person.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery',false)
+
+const url = process.env.MONGO_URI
+
+mongoose.connect(url)
+  .then(() => console.log('mongoose connected'))
+  .catch(error => console.log(error.message))
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
